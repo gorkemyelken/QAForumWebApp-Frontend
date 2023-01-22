8322de7b8642b301a9a7f53eaafa92253e1f7638
Görkem <gorkem_yelken@hotmail.com>
2023-01-22 22:02:12 +0300

    User activity oluşturuldu.

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,13 +1,41 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 import Avatar from "../Avatar/Avatar";
+import UserActivity from "../UserActivity/UserActivity";
+import "./User.scss";
 
 function User() {
-    const { userId } = useParams();
+    const { userId} = useParams();
+    const [user, setUser] = useState();
+
+    const getUser = () => {
+        fetch("/users/"+userId, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization" : localStorage.getItem("tokenKey"),
+            },
+          })
+          .then(res => res.json())
+          .then(
+              (result) => {
+                  console.log(result);
+                  setUser(result);
+              },
+              (error) => {
+                  console.log(error)
+              }
+          )
+          }
+
+        useEffect(() => {
+            getUser()
+        }, [])
+
     return(
-        <div>
-            User!! {userId}
-            <Avatar avatarId={0}/>
+        <div className="root">
+            {user? <Avatar avatarId={user.avatarId} /> : "" }
+            <UserActivity userId={userId} />
         </div>
     )
 }
